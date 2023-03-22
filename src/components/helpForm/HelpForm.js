afc570daa081e7146b7e71e4818a00fd03f778eb
ederramos01 <eder.ramos01@telusinternational.com>
2023-03-22 18:49:14 -0500

    project updated

--- src/components/helpForm/HelpForm.js
+++ src/components/helpForm/HelpForm.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./HelpForm.css";
 import Field from "../field/Field";
-import Button from "../buttons/Button";
 
 const HelpForm = (props) => {
   const handlerOnClick = () => {
@@ -9,12 +8,12 @@ const HelpForm = (props) => {
   };
 
   return (
-    <div className="helpform">
+    <div className="helpform" onClick={handlerOnClick}>
       <div className="modal">
         {props.data.map((item, index)=>(
           <Field key={index} {...item}/>
         ))}
-        <Button onShowModal={handlerOnClick}>Close</Button>
+
       </div>
     </div>
   );
