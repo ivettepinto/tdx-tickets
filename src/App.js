7346874cc202c5845060e28487d2836389ffa252
ederramos01 <eder.ramos01@telusinternational.com>
2023-03-22 14:38:55 -0500

    button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Button from "./components/buttons/Button";
+
 function App() {
+
+  const showModal = () => {
+    //se crea un useState tipo boolean
+    console.log('diste clic');
+  }
+
+  //aca debe de existir un useState booleano que te muestre el modal
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Button onShowModal={showModal}>Ayuda</Button>
+    </>
   );
 }
 
