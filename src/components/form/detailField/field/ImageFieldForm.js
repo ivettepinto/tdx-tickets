8b4f920398ff858824e41d888c1f388b69f12067
oliver-sierra-telus <oliver.sierra@telus.com>
2023-03-27 11:51:07 -0600

    ssss

---
+++ src/components/form/detailField/field/ImageFieldForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "../Form.css";
+
+const ImageField = (props) => {
+  return (
+    <>
+      <div className="form-field">
+      <label htmlFor="imageurl">Image Url</label>
+      <input
+        onChange={event => props.handleOnChange(props.index, event)}
+        name="imageurl"
+        id="imageurl"
+        type="text"
+        required={true}
+        value={props.imageurl}
+      />
+      </div>
+    </>
+  );
+};
+
+export default ImageField;
