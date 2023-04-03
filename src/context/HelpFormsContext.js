c951fdeac1ccc755f95aab122ba94f659dee45d6
ederramos01 <eder.ramos01@telusinternational.com>
2023-04-03 15:55:22 -0600

    Generated.json file fixed

--- src/context/HelpFormsContext.js
+++ src/context/HelpFormsContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
-import generatedJson from '../helpers/Generated.json';
+import generatedJson from '../helpers/Generated';
 
 const HelpContext = createContext();
 
